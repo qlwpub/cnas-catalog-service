3675f2cc71b9bd4254ddf35d548458e697c750b7
Liwen Qi <liwen.qi@billie.io>
2025-04-30 12:37:20 +0200

    added config to pull config data from a (remote) config server. Note all changes are build dependencies and config files.

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,15 +18,26 @@ repositories {
 	mavenCentral()
 }
 
+extra["springCloudVersion"] = "2024.0.1"
+
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.cloud:spring-cloud-starter-config")
+	implementation(kotlin("stdlib"))
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.boot:spring-boot-starter-webflux")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     implementation(kotlin("stdlib"))
+	implementation("org.springframework.retry:spring-retry")
 
 	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+	}
 }
 
 tasks.withType<Test> {
