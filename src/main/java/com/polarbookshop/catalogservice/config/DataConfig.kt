2bc75f2b3a8a3ad77bf5f4d98b3c2e303fcbaf95
Liwen Qi <liwen.qi@billie.io>
2025-05-31 20:55:51 +0200

    12.x.y: fix error in DataConfig.kt

--- src/main/java/com/polarbookshop/catalogservice/config/DataConfig.kt
+++ src/main/java/com/polarbookshop/catalogservice/config/DataConfig.kt
@@ -5,15 +5,20 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.data.domain.AuditorAware
 import org.springframework.data.jdbc.repository.config.EnableJdbcAuditing
 import org.springframework.security.core.context.SecurityContextHolder
+import java.util.*
 
 @Configuration
 @EnableJdbcAuditing
-class DataConfig{
+class DataConfig {
 
     @Bean
-    fun auditorAware() : AuditorAware<String> {
-        return AuditorAware<String> {
-            SecurityContextHolder.getContext()?.authentication.takeIf { it.isAuthenticated }?.name }
+    @Suppress("unused")
+    fun auditorAware(): AuditorAware<String> {
+        return AuditorAware {
+            Optional.ofNullable(SecurityContextHolder.getContext())
+                .map { it.authentication }
+                .filter { it.isAuthenticated }
+                .map { it.name }
         }
     }
 }
